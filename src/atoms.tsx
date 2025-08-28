4706dc04638b2dbe574513c33d29f59827c530d6
yyb <yyb0918@gmail.com>
2025-08-28 23:53:49 +0900

    first commit

---
+++ src/atoms.tsx
@@ -0,0 +1,34 @@
+import { atom, selector } from 'recoil';
+
+export const minutesState = atom({
+  key: 'minutes',
+  default: 0,
+});
+
+// 타입스크립트가 못찾으면 <number>
+export const hoursSelector = selector({
+  key: 'hours',
+  get: ({ get }) => {
+    const minutes = get(minutesState);
+    return minutes / 60;
+  },
+  set: ({ set }, newValue) => {
+    const minutes = Number(newValue) * 60;
+    set(minutesState, minutes);
+  },
+});
+
+export interface ITodo {
+  id: number;
+  text: string;
+}
+
+interface IToDoState {
+  [key: string]: ITodo[];
+}
+
+// Drag and Drop
+export const toDoState = atom<IToDoState>({
+  key: 'toDo',
+  default: {},
+});
